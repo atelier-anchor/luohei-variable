d13f406ee57961c6ee2d4d2f7fc1685afddbd12c
Xiangdong Zeng <xdzeng96@gmail.com>
2022-03-10 21:59:50 +0800

    gyroscope: support mouse move event

--- gyroscope/script.js
+++ gyroscope/script.js
@@ -8,14 +8,22 @@ const elemYwgt = document.getElementById('ywgt')
 const scale = (x, precision = 100) =>
   Math.round((100 - Math.cos((x * Math.PI) / 180) * 90) * precision) / precision
 
-const handleOrientation = (e) => {
-  const beta = Math.max(Math.min(e.beta, 90), -90)
-  const xwgt = scale(beta)
-  const ywgt = scale(e.gamma)
+const update = (xwgt, ywgt) => {
   const varSettings = `"XWGT" ${xwgt}, "YWGT" ${ywgt}`
   elemText.style.fontVariationSettings = varSettings
   elemXwgt.innerText = xwgt
   elemYwgt.innerText = ywgt
+}
+
+const handleOrientation = (e) => {
+  const beta = Math.max(Math.min(e.beta, 90), -90)
+  update(scale(beta), scale(e.gamma))
+}
+
+const handleMousemove = (e) => {
+  const x = Math.round((e.clientX / app.clientWidth) * 90 + 10)
+  const y = Math.round((1 - e.clientY / app.clientHeight) * 90 + 10)
+  update(x, y)
 }
 
 let initialized = false
@@ -30,3 +38,5 @@ app.addEventListener('click', (e) => {
     initialized = true
   }
 })
+
+app.addEventListener('mousemove', handleMousemove)
