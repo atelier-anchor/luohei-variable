ce0c2ddb7166da450d684468b59455d293e93f54
Xiangdong Zeng <xdzeng96@gmail.com>
2022-03-14 21:23:17 +0800

    rename; remove caption text; update microphone control

--- sensors/script.js
+++ sensors/script.js
@@ -1,19 +1,14 @@
 const app = document.getElementById('app')
-const toggler = document.getElementById('toggler')
+const togglerElem = document.getElementById('toggler')
 const statusElem = document.getElementById('status')
-
-const elemText = document.getElementById('text')
-const elemXwgt = document.getElementById('xwgt')
-const elemYwgt = document.getElementById('ywgt')
+const textElem = document.getElementById('text')
 
 const scale = (x, precision = 100) =>
   Math.round((100 - Math.cos((x * Math.PI) / 180) * 90) * precision) / precision
 
-const update = (xwgt, ywgt) => {
-  const varSettings = `"XWGT" ${xwgt}, "YWGT" ${ywgt}`
-  elemText.style.fontVariationSettings = varSettings
-  elemXwgt.innerText = xwgt
-  elemYwgt.innerText = ywgt
+const update = (x, y) => {
+  const varSettings = `"XWGT" ${x}, "YWGT" ${y}`
+  textElem.style.fontVariationSettings = varSettings
 }
 
 const handleOrientation = (e) => {
@@ -27,38 +22,40 @@ const handleMousemove = (e) => {
   update(x, y)
 }
 
-let requestID
-let audioContext
+let requestID = null
+let audioContext = null
 const handleMicrophone = async (e) => {
   if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false })
 
     audioContext = new AudioContext()
-    const mediaStreamAudioSourceNode = audioContext.createMediaStreamSource(stream)
-    const analyserNode = audioContext.createAnalyser()
-    mediaStreamAudioSourceNode.connect(analyserNode)
+    const mediaStreamAudioSource = audioContext.createMediaStreamSource(stream)
+    const analyser = audioContext.createAnalyser()
+    mediaStreamAudioSource.connect(analyser)
 
-    const pcmData = new Float32Array(analyserNode.fftSize)
+    const data = new Float32Array(analyser.fftSize)
     const onFrame = () => {
-      analyserNode.getFloatTimeDomainData(pcmData)
-      const rms = Math.sqrt(pcmData.reduce((a, b) => a + b * b, 0)) / pcmData.length
-      const val = Math.min(Math.round(10 + rms * 20000), 100)
-      const varSettings = elemText.style.fontVariationSettings.replace(/"XWGT" \d+/, `"XWGT" ${val}`)
-      elemText.style.fontVariationSettings = varSettings
-      elemXwgt.innerText = val
+      analyser.getFloatTimeDomainData(data)
+      const rms = Math.sqrt(data.reduce((a, b) => a + b * b, 0) / data.length)
+      const x = Math.min(Math.round(10 + rms * 200), 100)
+      textElem.style.fontVariationSettings = `"XWGT" ${x}, "YWGT" ${10}`
       requestID = window.requestAnimationFrame(onFrame)
     }
     requestID = window.requestAnimationFrame(onFrame)
+  } else {
+    console.warn('No microphone available.')
   }
 }
 
 const stopMicrophone = () => {
-  window.cancelAnimationFrame(requestID)
-  audioContext.close()
+  if (audioContext && requestID) {
+    audioContext.close()
+    window.cancelAnimationFrame(requestID)
+  }
 }
 
 let initialized = false
-app.addEventListener('click', (e) => {
+statusElem.addEventListener('click', (e) => {
   if (!initialized) {
     // Request permission for iOS 13+ devices
     if (DeviceMotionEvent && typeof DeviceMotionEvent.requestPermission === 'function') {
@@ -73,7 +70,7 @@ app.addEventListener('click', (e) => {
 app.addEventListener('mousemove', handleMousemove)
 
 let togglerState = false
-toggler.addEventListener('click', (e) => {
+togglerElem.addEventListener('click', () => {
   if (togglerState) {
     window.addEventListener('deviceorientation', handleOrientation)
     app.addEventListener('mousemove', handleMousemove)
@@ -84,5 +81,5 @@ toggler.addEventListener('click', (e) => {
     handleMicrophone()
   }
   togglerState = !togglerState
-  toggler.classList.toggle('active')
+  togglerElem.classList.toggle('active')
 })
