1c94e0554e3db0e04b66dc02b5f5347b0db9629e
Xiangdong Zeng <xdzeng96@gmail.com>
2022-03-23 14:50:18 +0800

    mobile view

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,22 +1,33 @@
 <template>
-  <header class="relative flex justify-between text-sm font-optical-sm px-8 sm:px-16 py-4 z-10">
-    <button v-if="isHome" class="tracking-1">观看动画</button>
-    <a v-else href="#/"><span class="tracking-1">络黑</span> LuoHei Variable</a>
-    <nav class="hidden sm:block">
-      <ul class="flex gap-4">
-        <li v-for="(item, name) in nav" :key="name" class="tracking-1">
-          <a :href="`#/${name}`" :class="{ 'font-bold': isActive(name) }">{{ item.title }}</a>
-        </li>
-      </ul>
-    </nav>
+  <header class="fixed w-full px-8 sm:px-16 py-4 z-10">
+    <div class="flex justify-between text-sm font-optical-sm">
+      <button v-if="isHome" class="tracking-1">观看动画</button>
+      <a v-else href="#/"><span class="tracking-1">络黑</span> LuoHei Variable</a>
+      <button class="sm:hidden tracking-1" @click="toggleNav()">页面</button>
+      <nav
+        class="absolute top-4 left-8 bg-light sm:static sm:block sm:bg-transparent"
+        :class="{ hidden: !showNav }"
+      >
+        <ul class="flex gap-4">
+          <li v-for="(item, name) in nav" :key="name" class="tracking-1">
+            <a :href="`#/${name}`" :class="{ 'font-bold': isActive(name) }">{{ item.title }}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 
 const isActive = (name) => props.currentPath.includes(name)
 const isHome = computed(() => props.currentPath === '' || props.currentPath === '#/')
+
+const showNav = ref(false)
+const toggleNav = () => (showNav.value = !showNav.value)
+
+window.addEventListener('resize', () => (showNav.value = false))
 
 const props = defineProps({
   nav: Object,
