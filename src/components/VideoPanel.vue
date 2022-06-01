d1997434458d20800c574ff47552df80c4124134
Xiangdong Zeng <xdzeng96@gmail.com>
2022-06-01 12:18:41 +0800

    always use dark mode for video panel

--- src/components/VideoPanel.vue
+++ src/components/VideoPanel.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="absolute inset-0 px-8 sm:px-16 z-10 bg-light dark:bg-dark">
+  <div class="absolute inset-0 px-8 sm:px-16 z-10 text-light bg-dark">
     <div class="relative flex items-center justify-center h-screen max-w-screen-2xl mx-auto">
       <div class="absolute top-0 right-0 py-4 text-sm font-optical-size-sm">
         <button @click="$emit('close-video')" class="tracking">关闭</button>
