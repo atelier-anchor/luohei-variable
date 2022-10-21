4941505636f938174d9e355c1306d30aecb158b9
Xiangdong Zeng <xdzeng96@gmail.com>
2022-10-21 20:13:56 +0800

    about: use space for en date

--- src/components/about/AboutDate.vue
+++ src/components/about/AboutDate.vue
@@ -1,11 +1,17 @@
 <template>
   <p>
-    {{ $t('about.last-updated') }}<span class="cjk-latin-glue"></span>
+    <template v-if="isLocaleZh">
+      {{ $t('about.last-updated') }}<span class="cjk-latin-glue"></span>
+    </template>
+    <template v-else>
+      {{ $t('about.last-updated') + ' ' }}
+    </template>
     <time :datetime="date.toISOString()" v-html="cjkKern($d(date, 'long'))"></time>
   </p>
 </template>
 
 <script setup>
+import { isLocaleZh } from '@/i18n'
 import { cjkKern } from '@/utils'
 
 const date = new Date(import.meta.env.VITE_BUILD_DATE)
