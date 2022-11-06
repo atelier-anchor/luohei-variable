d50b8f1f68039f5ba1f25e8d3dbe87660c3a65e4
Xiangdong Zeng <xdzeng96@gmail.com>
2022-11-06 16:35:13 +0800

    fix `no-break`

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,10 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 const plugin = require('tailwindcss/plugin')
 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.vue'],
+  safelist: ['no-break'],
   theme: {
     colors: {
       current: 'currentColor',
